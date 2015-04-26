96ef184db186fa5e0da3775b7094d89b77c6b976
Jarroba Web <jarrobaweb@gmail.com>
2015-04-26 19:45:27 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Richard'
 
-# Tengo que instalar el beautiful soup: pip install beautifulsoup4  & pip install requests
-#
-# En este proyecto voy a scrapear resulados de partidos de futbol con la siguiente información
-#   1.- Temporada
-#   2.- Jornada
-#   3.- Equipos
-#   4.- Resultado
-#   5.- Fecha
-#
-# Para ello voy a obtener los resultados de dos webs:
-#   1º.- http://www.bdfutbol.com                -> Obtengo los resultados de las temporadas anteriores
-#   2º.- http://www.resultados-futbol.com       -> Obtengo los resultado de la presente temporada 2014-15
-#
-#   También voy a obtener en ficheros los partidos de cada jornada para poder predecir sus resultados
-
-
 import ScrapBDFutbol as bdFutbol
 import ScrapTemporada2014 as thisTemporada
 
